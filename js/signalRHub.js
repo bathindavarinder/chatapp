7d69d9bf7e73bfe2bb92dbb047a4874c4bd1a95f
bathindavarinder <bathindavarinder@gmail.com>
2014-12-25 16:08:55 +0530

    ad

--- js/signalRHub.js
+++ js/signalRHub.js
@@ -116,30 +116,40 @@
     });
 
     $.connection.hub.reconnected(function () {
+
+        var myClientId = $.connection.hub.id;
+
+        if (myClientId != localStorage.get("ConnId")) {
+            var yourname = localStorage.getItem("Name");
+            chat.server.updateConnId(localStorage.get("ConnId"), myClientId, yourname);
+            localStorage.setItem("ConnId", myClientId);
+        }
         show('afui', true);
         show('loading', false);
     });
 
     $.connection.hub.disconnected(function () {
 
-
         if (!window.background) {
 
-           
             $.connection.hub.start().done(function () {
+
                 var myClientId = $.connection.hub.id;
+
                 localStorage.setItem("ConnId", myClientId);
                 //chat.server.updateName(myClientId, $('#displayname').val());
+
                 var name = localStorage.getItem("Name");
+
                 var room = localStorage.getItem("room");
 
 
                 $.JoinRoom(room, name);
-               
+
             });
-            
+
         } else {
-            $.showNotification("Timeout","You have been pulled out of room because of no activity");
+            $.showNotification("Timeout", "You have been pulled out of room because of no activity");
             $.openRooms();
         }
 
@@ -151,14 +161,18 @@
     };
 
     chat.client.addChatMessage = function (message) {
+
         var n = message.indexOf(":");
+
         var name = message.substring(0, n);
+
         var encodedMsg = $('<div />').text(message).html();
 
         var msg = $('<li>' + encodedMsg + '</li>');
-        $("#ChatWindow").append(msg);
-        msg.focus();
 
+        $("#ChatWindow").append(msg);
+
+        msg.focus();
 
         if (window.background) {
             $.showNotification(name, encodedMsg);
@@ -175,45 +189,66 @@
     }
 
     document.addEventListener("offline", onOffline, false);
+
     function onOffline() {
         alert("please check your network.")
         $.openRooms();
     }
 
     $.leftMessage = function (message, by, left) {
+
         if ($('div#' + by).length == 0) {
 
             if (!left) {
-                var source = $("#personal-template").html();
-                var template = Handlebars.compile(source);
-                var context = { name: by }
-                var html = template(context);
-                var parentDiv = $("<div  class='panel chwin' style='display:none;height: 85%;position:static' id='" + by + "'></div>");
 
-                parentDiv.html(html);
+                var parentDiv = $.buildChatWindow(by);
 
                 $('#content').append(parentDiv);
 
                 var encodedMsg = $('<div />').text(message).html();
+
                 var msg = $('<li>' + by + ' : ' + encodedMsg + '</li>');
+
                 $('div#' + by + ' .ChatWindow').append(msg);
+
                 msg.focus();
+
                 if (window.activeUser != by)
                     $('#userList #' + by).css("background-color", "orange");
             }
         }
         else {
+
             if (window.activeUser != by)
                 $('#userList #' + by).css("background-color", "orange");
 
             var encodedMsg = $('<div />').text(message).html();
+
             var msg = $('<li>' + by + ' : ' + encodedMsg + '</li>');
+
             $('div#' + by + ' .ChatWindow').append(msg);
+
             msg.focus();
 
         }
     }
 
+    $.buildChatWindow = function (id) {
+
+        var source = $("#personal-template").html();
+
+        var template = Handlebars.compile(source);
+
+        var context = { name: by }
+
+        var html = template(context);
+
+        var parentDiv = $("<div  class='panel chwin' style='display:none;height: 85%;position:static' id='" + id + "'></div>");
+
+        parentDiv.html(html);
+
+        return parentDiv;
+    }
     chat.client.recievePersonalChat = function (message, by) {
 
         $.leftMessage(message, by, false);
@@ -229,13 +264,7 @@
 
         if ($('div#' + by).length == 0) {
 
-            var source = $("#personal-template").html();
-            var template = Handlebars.compile(source);
-            var context = { name: by }
-            var html = template(context);
-            var parentDiv = $("<div  class='panel chwin' style='display:none;height: 85%;position:static' id='" + by + "'></div>");
-
-            parentDiv.html(html);
+            var parentDiv = $.buildChatWindow(by);
 
             $('#content').append(parentDiv);
 
@@ -246,11 +275,13 @@
             msg.focus();
 
             if (window.activeUser != by)
-                $('#userList #' + by).css("background-color", "orange");
+                $('#userList #' + by).parent().css("background-color", "orange");
+
         }
         else {
+
             if (window.activeUser != by)
-                $('#userList #' + by).css("background-color", "orange");
+                $('#userList #' + by).parent().css("background-color", "orange");
 
             var encodedMsg = $('<div />').text(message).html();
 
@@ -272,7 +303,79 @@
     }
 
 
+    $.userClick = function (clickId) {
 
+        var username = clickId;// $(this).attr("id");
+
+        var room = localStorage.getItem("room");
+
+        var name = localStorage.getItem("Name");
+
+        if (username == name || username == "Back") {
+            return;
+        }
+
+        if (username == "Home") {
+
+            $.each($('.chwin'), function (name) {
+                $(this).css("display", "none");
+            });
+            $("#HeadName").text(room);
+            $('#RoomChatWindow').css("display", "block");
+            window.activeUser = "";
+            return;
+        }
+
+        if ($('div#' + username).length == 0) {
+
+            var parentDiv = $.buildChatWindow(username);
+
+            $('#RoomChatWindow').css("display", "none");
+
+            $("#HeadName").text(username);
+
+            $.each($('.chwin'), function (name) {
+                $(this).css("display", "none");
+            });
+            parentDiv.html(html);
+            window.activeUser = username;
+            $('#content').append(parentDiv);
+
+            $('#userList #' + username).css("background-color", "#619ef2");
+        }
+        else {
+
+            $.each($('.chwin'), function (name) {
+                $(this).css("display", "none");
+            });
+
+            $('#RoomChatWindow').css("display", "none");
+
+            window.activeUser = username;
+            $("#HeadName").text(username);
+            $('div#' + username).css("display", "block");
+            $('#userList #' + username).css("background-color", "#619ef2");
+        }
+        $.ui.toggleSideMenu();
+    }
+
+
+    $.SendMessage = function () {
+        var Message = $("#HomeMessage").val();
+
+        if (Message == "") {
+            return;
+        }
+
+        var room = localStorage.getItem("room");
+        if (window.activeUser == "") {
+            $.SendGroupMessage(room, name, Message);
+        } else {
+            $.SendPersonalMessage(window.activeUser, Message, name);
+        }
+
+        $("#HomeMessage").val("");
+    }
 
 }(jQuery));
 
